dfb5865ae447b875e8d67a732db288771a85a5ce
PeterParker <107173341+Peterparker0296@users.noreply.github.com>
2022-11-16 12:16:10 +0530

    Add files via upload
    MultiTable Wcf

---
+++ Multiwcf/Multiwcf/IService1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Multiwcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "GetEmp", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        List<Employeesss> GetEmployees();
+
+
+
+
+
+    }
+
+
+
+
+
+    [DataContract]
+    public class Employeesss
+    {
+
+        [DataMember]
+        public int EmployeeId { get; set; }
+
+        [DataMember]
+        public string EmpName { get; set; }
+
+        [DataMember]
+        public int DeptID { get; set; }
+
+        [DataMember]
+        public string City { get; set; }
+
+
+        [DataMember]
+        public string Course { get; set; }
+
+
+        [DataMember]
+
+        public int Marks { get; set; }
+
+    }
+
+
+    
+
+
+ 
+   
+   
+}
---
+++ Multiwcf/Multiwcf/Service1.svc.cs
@@ -0,0 +1,76 @@
+﻿using Multiwcf.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Multiwcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public List<Employeesss> GetEmployees()
+        {
+
+            var db1 = new TestDBEntities();
+            var db2 = new SampleProductEntities();
+
+            var Emp = db1.Employeessses.ToList();
+            var Dept = db2.Departments.ToList();
+            //  var de = db2.Departments.ToList();
+
+            var DE = db2.Departments.ToList();
+
+
+
+            var task = (from Employeesss in Emp
+                       
+                        join Department in Dept
+                        on new { X1 = Employeesss.EmployeeID , X2 = Employeesss.DeptID } equals new {X1 = Department.EmployeeID , X2 = Department.DeptID}
+
+
+                        where Employeesss.Marks > 67
+
+                        orderby Employeesss.EmpName descending
+
+                     
+                         select Employeesss).AsEnumerable().Select(x => new Employeesss()
+                         {
+
+                             EmployeeId = x.EmployeeID,
+
+                             EmpName = x.EmpName,
+
+                             DeptID = x.DeptID ,
+
+                             Course = x.Course ,
+
+                             City = x.City,
+
+                             
+                             Marks = (int)x.Marks
+
+
+                             
+
+                         }
+                        ).Take(2).ToList();
+
+            return task;
+
+
+
+
+
+
+          
+
+
+
+        }
+    }
+}
